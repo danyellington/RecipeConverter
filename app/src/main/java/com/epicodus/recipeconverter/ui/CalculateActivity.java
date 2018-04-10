9a7f8b18b4ff3a7c91ec1373c17121c8b067c3e2
Danyel Murray <danyelmurray1987@gmail.com>
2018-04-10 11:35:46 -0700

    multiply numbers between activities calculate and results.

--- app/src/main/java/com/epicodus/recipeconverter/ui/CalculateActivity.java
+++ app/src/main/java/com/epicodus/recipeconverter/ui/CalculateActivity.java
@@ -3,10 +3,12 @@ package com.epicodus.recipeconverter.ui;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.TextView;
 
 import com.epicodus.recipeconverter.R;
 
@@ -18,23 +20,34 @@ public class CalculateActivity extends AppCompatActivity {
         setContentView(R.layout.activity_calculate);
         Button calculateButton = (Button) findViewById(R.id.calculateButton);
 
+
         calculateButton.setOnClickListener(new View.OnClickListener() {
-
-
             @Override
             public void onClick(View v) {
+
+                final EditText value1=(EditText) findViewById(R.id.calculateAmount);
+                final EditText value2=(EditText) findViewById(R.id.quantity);
+                final TextView result=(TextView) findViewById (R.id.quantity);
+
+                    int val1=Integer.parseInt(value1.getText().toString());
+                    int val2=Integer.parseInt(value2.getText().toString());
+                    Integer answer = val1*val2;
+                    result.setText(answer.toString());
+
+
+                Intent intent = new Intent(CalculateActivity.this, ResultsActivity.class);
                 EditText etRecipe = (EditText) findViewById(R.id.calculate);
                 EditText etAmount = (EditText) findViewById(R.id.calculateAmount);
                 EditText etQuantity = (EditText) findViewById(R.id.quantity);
-                Intent intent = new Intent(CalculateActivity.this, ResultsActivity.class);
-
                 intent.putExtra("ingredient", etRecipe.getText().toString());
                 intent.putExtra("amount", etAmount.getText().toString());
                 intent.putExtra("quantity", etQuantity.getText().toString());
                 startActivity(intent);
             }
         });
-    }
+        }
+
+
 
         public void onRadioButtonClicked(View view) {
             // Is the button now checked?
