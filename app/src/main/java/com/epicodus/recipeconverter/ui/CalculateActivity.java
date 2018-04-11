dd526f2f0c3e2f0905b7dbf7af9d2d623031fe2b
Danyel Murray <danyelmurray1987@gmail.com>
2018-04-11 15:29:09 -0700

    mvp. app multiplies and divides recipe in most basic form.

--- app/src/main/java/com/epicodus/recipeconverter/ui/CalculateActivity.java
+++ app/src/main/java/com/epicodus/recipeconverter/ui/CalculateActivity.java
@@ -27,28 +27,99 @@ public class CalculateActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                final EditText value1=(EditText) findViewById(R.id.calculateAmount);
-                final EditText value2=(EditText) findViewById(R.id.quantity);
-                final TextView result=(TextView) findViewById (R.id.quantity);
+                final EditText value1 = (EditText) findViewById(R.id.quantity);
+                final EditText value2 = (EditText) findViewById(R.id.calculateAmount);
+                final EditText value3 = (EditText) findViewById(R.id.quantity2);
+                final EditText value4 = (EditText) findViewById(R.id.quantity3);
+                final EditText value5 = (EditText) findViewById(R.id.quantity4);
+                final EditText value6 = (EditText) findViewById(R.id.quantity5);
+                final EditText value7 = (EditText) findViewById(R.id.quantity6);
+                final EditText value8 = (EditText) findViewById(R.id.quantity7);
+                final EditText value9 = (EditText) findViewById(R.id.quantity8);
 
-                    int val1=Integer.parseInt(value1.getText().toString());
-                    int val2=Integer.parseInt(value2.getText().toString());
-                    Integer answer = val1*val2;
-                    result.setText(answer.toString());
+                final TextView result = (TextView) findViewById(R.id.quantity);
+                final TextView result2 = (TextView) findViewById(R.id.quantity2);
+                final TextView result3 = (TextView) findViewById(R.id.quantity3);
+                final TextView result4 = (TextView) findViewById(R.id.quantity4);
+                final TextView result5 = (TextView) findViewById(R.id.quantity5);
+                final TextView result6 = (TextView) findViewById(R.id.quantity6);
+                final TextView result7 = (TextView) findViewById(R.id.quantity7);
+                final TextView result8 = (TextView) findViewById(R.id.quantity8);
 
+
+                int val1 = Integer.parseInt(value1.getText().toString());
+                int val2 = Integer.parseInt(value2.getText().toString());
+                int val3 = Integer.parseInt(value3.getText().toString());
+                int val4 = Integer.parseInt(value4.getText().toString());
+                int val5 = Integer.parseInt(value5.getText().toString());
+                int val6 = Integer.parseInt(value6.getText().toString());
+                int val7 = Integer.parseInt(value7.getText().toString());
+                int val8 = Integer.parseInt(value8.getText().toString());
+                int val9 = Integer.parseInt(value9.getText().toString());
+
+
+                Integer answer = val1 * val2;
+                Integer answer2 = val3 * val2;
+                Integer answer3 = val4 * val2;
+                Integer answer4 = val5 * val2;
+                Integer answer5 = val6 * val2;
+                Integer answer6 = val7 * val2;
+                Integer answer7 = val8 * val2;
+                Integer answer8 = val9 * val2;
+
+                result.setText(answer.toString());
+                result2.setText(answer2.toString());
+                result3.setText(answer3.toString());
+                result4.setText(answer4.toString());
+                result5.setText(answer5.toString());
+                result6.setText(answer6.toString());
+                result7.setText(answer7.toString());
+                result8.setText(answer8.toString());
 
 
                 Intent intent = new Intent(CalculateActivity.this, ResultsActivity.class);
-                EditText etRecipe = (EditText) findViewById(R.id.calculate);
+                EditText etRecipe = (EditText) findViewById(R.id.ingredient);
+                EditText etRecipe2 = (EditText) findViewById(R.id.ingredient2);
+                EditText etRecipe3 = (EditText) findViewById(R.id.ingredient3);
+                EditText etRecipe4 = (EditText) findViewById(R.id.ingredient4);
+                EditText etRecipe5 = (EditText) findViewById(R.id.ingredient5);
+                EditText etRecipe6 = (EditText) findViewById(R.id.ingredient6);
+                EditText etRecipe7 = (EditText) findViewById(R.id.ingredient7);
+                EditText etRecipe8 = (EditText) findViewById(R.id.ingredient8);
+
                 EditText etUnit = (EditText) findViewById(R.id.units);
-                //EditText etAmount = (EditText) findViewById(R.id.calculateAmount);
                 EditText etQuantity = (EditText) findViewById(R.id.quantity);
+                EditText etQuantity2 = (EditText) findViewById(R.id.quantity2);
+                EditText etQuantity3 = (EditText) findViewById(R.id.quantity3);
+                EditText etQuantity4 = (EditText) findViewById(R.id.quantity4);
+                EditText etQuantity5 = (EditText) findViewById(R.id.quantity5);
+                EditText etQuantity6 = (EditText) findViewById(R.id.quantity6);
+                EditText etQuantity7 = (EditText) findViewById(R.id.quantity7);
+                EditText etQuantity8 = (EditText) findViewById(R.id.quantity8);
+
+
+
                 EditText etNotes = (EditText) findViewById(R.id.notes);
                 intent.putExtra("ingredient", etRecipe.getText().toString());
+                intent.putExtra("ingredient2", etRecipe2.getText().toString());
+                intent.putExtra("ingredient3", etRecipe3.getText().toString());
+                intent.putExtra("ingredient4", etRecipe4.getText().toString());
+                intent.putExtra("ingredient5", etRecipe5.getText().toString());
+                intent.putExtra("ingredient6", etRecipe6.getText().toString());
+                intent.putExtra("ingredient7", etRecipe7.getText().toString());
+                intent.putExtra("ingredient8", etRecipe8.getText().toString());
+
                 intent.putExtra("units", etUnit.getText().toString());
-               // intent.putExtra("amount", etAmount.getText().toString());
-                intent.putExtra("quantity", etQuantity.getText().toString());
                 intent.putExtra("notes", etNotes.getText().toString());
+
+                intent.putExtra("quantity", etQuantity.getText().toString());
+                intent.putExtra("quantity2", etQuantity2.getText().toString());
+                intent.putExtra("quantity3", etQuantity3.getText().toString());
+                intent.putExtra("quantity4", etQuantity4.getText().toString());
+                intent.putExtra("quantity5", etQuantity5.getText().toString());
+                intent.putExtra("quantity6", etQuantity6.getText().toString());
+                intent.putExtra("quantity7", etQuantity7.getText().toString());
+                intent.putExtra("quantity8", etQuantity8.getText().toString());
                 startActivity(intent);
             }
         });
@@ -59,49 +130,103 @@ public class CalculateActivity extends AppCompatActivity {
 
                 final EditText value1 = (EditText) findViewById(R.id.quantity);
                 final EditText value2 = (EditText) findViewById(R.id.calculateAmount);
+                final EditText value3 = (EditText) findViewById(R.id.quantity2);
+                final EditText value4 = (EditText) findViewById(R.id.quantity3);
+                final EditText value5 = (EditText) findViewById(R.id.quantity4);
+                final EditText value6 = (EditText) findViewById(R.id.quantity5);
+                final EditText value7 = (EditText) findViewById(R.id.quantity6);
+                final EditText value8 = (EditText) findViewById(R.id.quantity7);
+                final EditText value9 = (EditText) findViewById(R.id.quantity8);
+
                 final TextView result = (TextView) findViewById(R.id.quantity);
+                final TextView result2 = (TextView) findViewById(R.id.quantity2);
+                final TextView result3 = (TextView) findViewById(R.id.quantity3);
+                final TextView result4 = (TextView) findViewById(R.id.quantity4);
+                final TextView result5 = (TextView) findViewById(R.id.quantity5);
+                final TextView result6 = (TextView) findViewById(R.id.quantity6);
+                final TextView result7 = (TextView) findViewById(R.id.quantity7);
+                final TextView result8 = (TextView) findViewById(R.id.quantity8);
 
 
                 int val1 = Integer.parseInt(value1.getText().toString());
                 int val2 = Integer.parseInt(value2.getText().toString());
+                int val3 = Integer.parseInt(value3.getText().toString());
+                int val4 = Integer.parseInt(value4.getText().toString());
+                int val5 = Integer.parseInt(value5.getText().toString());
+                int val6 = Integer.parseInt(value6.getText().toString());
+                int val7 = Integer.parseInt(value7.getText().toString());
+                int val8 = Integer.parseInt(value8.getText().toString());
+                int val9 = Integer.parseInt(value9.getText().toString());
+
+
                 Integer answer = val1 / val2;
+                Integer answer2 = val3 / val2;
+                Integer answer3 = val4 / val2;
+                Integer answer4 = val5 / val2;
+                Integer answer5 = val6 / val2;
+                Integer answer6 = val7 / val2;
+                Integer answer7 = val8 / val2;
+                Integer answer8 = val9 / val2;
+
                 result.setText(answer.toString());
+                result2.setText(answer2.toString());
+                result3.setText(answer3.toString());
+                result4.setText(answer4.toString());
+                result5.setText(answer5.toString());
+                result6.setText(answer6.toString());
+                result7.setText(answer7.toString());
+                result8.setText(answer8.toString());
+
 
                 Intent intent = new Intent(CalculateActivity.this, ResultsActivity.class);
-                EditText etRecipe = (EditText) findViewById(R.id.calculate);
+                EditText etRecipe = (EditText) findViewById(R.id.ingredient);
+                EditText etRecipe2 = (EditText) findViewById(R.id.ingredient2);
+                EditText etRecipe3 = (EditText) findViewById(R.id.ingredient3);
+                EditText etRecipe4 = (EditText) findViewById(R.id.ingredient4);
+                EditText etRecipe5 = (EditText) findViewById(R.id.ingredient5);
+                EditText etRecipe6 = (EditText) findViewById(R.id.ingredient6);
+                EditText etRecipe7 = (EditText) findViewById(R.id.ingredient7);
+                EditText etRecipe8 = (EditText) findViewById(R.id.ingredient8);
+
                 EditText etUnit = (EditText) findViewById(R.id.units);
-              //  EditText etAmount = (EditText) findViewById(R.id.calculateAmount);
                 EditText etQuantity = (EditText) findViewById(R.id.quantity);
+                EditText etQuantity2 = (EditText) findViewById(R.id.quantity2);
+                EditText etQuantity3 = (EditText) findViewById(R.id.quantity3);
+                EditText etQuantity4 = (EditText) findViewById(R.id.quantity4);
+                EditText etQuantity5 = (EditText) findViewById(R.id.quantity5);
+                EditText etQuantity6 = (EditText) findViewById(R.id.quantity6);
+                EditText etQuantity7 = (EditText) findViewById(R.id.quantity7);
+                EditText etQuantity8 = (EditText) findViewById(R.id.quantity8);
+
+
+
                 EditText etNotes = (EditText) findViewById(R.id.notes);
                 intent.putExtra("ingredient", etRecipe.getText().toString());
+                intent.putExtra("ingredient2", etRecipe2.getText().toString());
+                intent.putExtra("ingredient3", etRecipe3.getText().toString());
+                intent.putExtra("ingredient4", etRecipe4.getText().toString());
+                intent.putExtra("ingredient5", etRecipe5.getText().toString());
+                intent.putExtra("ingredient6", etRecipe6.getText().toString());
+                intent.putExtra("ingredient7", etRecipe7.getText().toString());
+                intent.putExtra("ingredient8", etRecipe8.getText().toString());
+
                 intent.putExtra("units", etUnit.getText().toString());
-               // intent.putExtra("amount", etAmount.getText().toString());
-                intent.putExtra("quantity", etQuantity.getText().toString());
                 intent.putExtra("notes", etNotes.getText().toString());
+
+                intent.putExtra("quantity", etQuantity.getText().toString());
+                intent.putExtra("quantity2", etQuantity2.getText().toString());
+                intent.putExtra("quantity3", etQuantity3.getText().toString());
+                intent.putExtra("quantity4", etQuantity4.getText().toString());
+                intent.putExtra("quantity5", etQuantity5.getText().toString());
+                intent.putExtra("quantity6", etQuantity6.getText().toString());
+                intent.putExtra("quantity7", etQuantity7.getText().toString());
+                intent.putExtra("quantity8", etQuantity8.getText().toString());
                 startActivity(intent);
             }
         });
 
 
-        }
-
-
-
-//        public void onRadioButtonClicked(View view) {
-//            // Is the button now checked?
-//            boolean checked = ((RadioButton) view).isChecked();
-//
-//            // Check which radio button was clicked
-//            switch(view.getId()) {
-//                case R.id.radio_multiply:
-//                    if (checked)
-//                        // Pirates are the best
-//                        break;
-//                case R.id.radio_divide:
-//                    if (checked)
-//                        // Ninjas rule
-//                        break;
-//            }
-//        }
     }
+
+}
 
--- app/src/main/java/com/epicodus/recipeconverter/ui/ResultsActivity.java
+++ app/src/main/java/com/epicodus/recipeconverter/ui/ResultsActivity.java
@@ -29,45 +29,73 @@ public class ResultsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_results);
 
 
-        TextView results = (TextView) findViewById(R.id.results);
+        TextView results = (TextView) findViewById(R.id.ingredient);
+        TextView results2 = (TextView) findViewById(R.id.ingredient2);
+        TextView results3 = (TextView) findViewById(R.id.ingredient3);
+        TextView results4 = (TextView) findViewById(R.id.ingredient4);
+        TextView results5 = (TextView) findViewById(R.id.ingredient5);
+        TextView results6 = (TextView) findViewById(R.id.ingredient6);
+        TextView results7 = (TextView) findViewById(R.id.ingredient7);
+        TextView results8 = (TextView) findViewById(R.id.ingredient8);
+
+
         TextView units = (TextView) findViewById(R.id.units);
-        TextView quantity = (TextView) findViewById(R.id.quantity);
         TextView notes = (TextView) findViewById(R.id.notes);
         notes.setMovementMethod(new ScrollingMovementMethod());
+
+        TextView quantity = (TextView) findViewById(R.id.quantity);
+        TextView quantity2 = (TextView) findViewById(R.id.quantity2);
+        TextView quantity3 = (TextView) findViewById(R.id.quantity3);
+        TextView quantity4 = (TextView) findViewById(R.id.quantity4);
+        TextView quantity5 = (TextView) findViewById(R.id.quantity5);
+        TextView quantity6 = (TextView) findViewById(R.id.quantity6);
+        TextView quantity7 = (TextView) findViewById(R.id.quantity7);
+        TextView quantity8 = (TextView) findViewById(R.id.quantity8);
 
 
 
         Intent intent = getIntent();
         String str = intent.getStringExtra("ingredient");
+        String str2 = intent.getStringExtra("ingredient2");
+        String str3 = intent.getStringExtra("ingredient3");
+        String str4 = intent.getStringExtra("ingredient4");
+        String str5 = intent.getStringExtra("ingredient5");
+        String str6 = intent.getStringExtra("ingredient6");
+        String str7 = intent.getStringExtra("ingredient7");
+        String str8 = intent.getStringExtra("ingredient8");
+
         String strU = intent.getStringExtra("units");
-        String strQ = intent.getStringExtra("quantity");
         String strN = intent.getStringExtra("notes");
+        String strQ = intent.getStringExtra("quantity");
+        String strQ2 = intent.getStringExtra("quantity2");
+        String strQ3 = intent.getStringExtra("quantity3");
+        String strQ4 = intent.getStringExtra("quantity4");
+        String strQ5 = intent.getStringExtra("quantity5");
+        String strQ6 = intent.getStringExtra("quantity6");
+        String strQ7 = intent.getStringExtra("quantity7");
+        String strQ8 = intent.getStringExtra("quantity8");
+
+
 
         results.setText(str);
-        units.setText(strU);
-        quantity.setText(strQ);
-        notes.setText(strN);
-    }
+        results2.setText(str2);
+        results3.setText(str3);
+        results4.setText(str4);
+        results5.setText(str5);
+        results6.setText(str6);
+        results7.setText(str7);
+        results8.setText(str8);
 
-//    @Override
-//    public void onClick(View v) {
-//
-//        if (v == mSaveRecipe) {
-//            FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-//            String uid = user.getUid();
-//
-//            DatabaseReference forecastRef = FirebaseDatabase
-//                    .getInstance()
-//                    .getReference(Constants.FIREBASE_CHILD_RECIPE)
-//                    .child(uid);
-//
-//            DatabaseReference pushRef = forecastRef.push();
-//            String pushId = pushRef.getKey();
-//            mRecipe.setPushId(pushId);
-//            pushRef.setValue(mRecipe);
-//
-//            Toast.makeText(getContext(), "Saved", Toast.LENGTH_SHORT).show();
-//        }
-//
-//    }
+        units.setText(strU);
+        notes.setText(strN);
+        quantity.setText(strQ);
+        quantity2.setText(strQ2);
+        quantity3.setText(strQ3);
+        quantity4.setText(strQ4);
+        quantity5.setText(strQ5);
+        quantity6.setText(strQ6);
+        quantity7.setText(strQ7);
+        quantity8.setText(strQ8);
+
+    }
 }
